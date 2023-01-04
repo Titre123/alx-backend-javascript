f16dd7179175259e1e30df0ddc5059a45de5ea78
Taiwo <taiwotriumohant@gmail.com>
2023-01-04 02:23:21 +0100

    new

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,7 +2,7 @@ export default function getListStudentIds(array) {
   if (Array.isArray(array) == false) {
     return [];
   }
-  else{
-      return array.map(item => item.id);
+  else {
+    return array.map(item => item.id);
   }
 }
