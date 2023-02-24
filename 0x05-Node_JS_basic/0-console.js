95055d8b4febd23cc018fc878508a451efa20498
Taiwo <taiwotriumohant@gmail.com>
2023-02-24 18:34:01 +0100

    nodejs

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -2,5 +2,7 @@
   Function takes in an argument and print it to stdout
 */
 function displayMessage(args) {
-  process.stdout.write(args)
+    process.stdout.write(args);
 }
+
+module.exports = displayMessage;
